197c4014f7a04c47fe309b1c6a3aa0c810b3d490
Arish N <136902412+ArishN@users.noreply.github.com>
2024-07-22 10:16:50 +0530

    files uploaded

---
+++ Electricals/script.js
@@ -0,0 +1,42 @@
+const scrollTop = document.querySelector('.scroll-top');
+if (scrollTop) {
+  const togglescrollTop = function() {
+    window.scrollY > 100 ? scrollTop.classList.add('active') : scrollTop.classList.remove('active');
+  }
+  window.addEventListener('load', togglescrollTop);
+  document.addEventListener('scroll', togglescrollTop);
+  scrollTop.addEventListener('click', window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  }));
+}
+
+const preloader = document.querySelector('#preloader');
+if (preloader) {
+  window.addEventListener('load', () => {
+    preloader.remove();
+  });
+}
+
+
+let navbarlinks = document.querySelectorAll('#navbar a');
+
+function navbarlinksActive() {
+  navbarlinks.forEach(navbarlink => {
+
+    if (!navbarlink.hash) return;
+
+    let section = document.querySelector(navbarlink.hash);
+    if (!section) return;
+
+    let position = window.scrollY + 200;
+
+    if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+      navbarlink.classList.add('active');
+    } else {
+      navbarlink.classList.remove('active');
+    }
+  })
+}
+window.addEventListener('load', navbarlinksActive);
+document.addEventListener('scroll', navbarlinksActive);
